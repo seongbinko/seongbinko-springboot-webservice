8a922eec7cb99faaf2765759a8a6185d7fb53507
SEONGBIN KO <wnrhd1082@gmail.com>
2020-03-14 17:15:40 +0900

    - 머스테치로 화면 구현 - js/css 선언 위치를 다르게 하여 웹사이트의 로딩 속도 향상 - js 객체를 이용하여 브라우저의 전역 변수 충돌 문제를 회피하도록 구현

--- src/main/java/com/seongbinko/springboot/service/posts/PostsService.java
+++ src/main/java/com/seongbinko/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.seongbinko.springboot.service.posts;
 
 import com.seongbinko.springboot.domain.posts.Posts;
 import com.seongbinko.springboot.domain.posts.PostsRepository;
+import com.seongbinko.springboot.web.dto.PostListResponseDto;
 import com.seongbinko.springboot.web.dto.PostUpdateReuqestDto;
 import com.seongbinko.springboot.web.dto.PostsResponseDto;
 import com.seongbinko.springboot.web.dto.PostsSaveRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -32,6 +36,20 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 계시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+    @Transactional(readOnly = true)
+    public List<PostListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc()
+                .stream()
+                .map(PostListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id" + id));
+
+        postsRepository.delete(posts);
     }
 
 
