8ce081d8304219e5d7e0a88d2ac8dab9c94366dc
SEONGBIN KO <wnrhd1082@gmail.com>
2020-03-31 11:26:03 +0900

    화면 구현 개선

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -1,4 +1,4 @@
-$('#index-title').on('click',function(){
+$('#index-tbody tr td:eq(1)').on('click',function(){
     if(!$('#user').text()) {
         alert('로그인 후 이용가능 합니다.');
         return false;
